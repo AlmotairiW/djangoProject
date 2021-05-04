09b91935df2b1e117ae542d4bcc17c3f05ec84d9
Waleed Almotairi <AlmotairiWal@gmail.com>
2021-05-04 21:56:24 +0300

    added

--- art_proj/art_app/urls.py
+++ art_proj/art_app/urls.py
@@ -2,4 +2,11 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index),
+    path('login_reg', views.login_reg), # render registration template
+    path('process_reg', views.process_reg), # adds user to DB
+    path('login', views.login), # log in, gets user from DB then redirect to sucsess
+    path('sucsess', views.sucsess), # to render appropriate template for artist/customer
+    path('add_item', views.add_item), # renders add item page for artist
+    path('create_art', views.create_art), # adds artwork to DB
+
 ]
