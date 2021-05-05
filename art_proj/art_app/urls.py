7b5d1687b5ea5d18405c849f5331abae69d0185c
Waleed Almotairi <AlmotairiWal@gmail.com>
2021-05-05 12:16:43 +0300

    updated urls

--- art_proj/art_app/urls.py
+++ art_proj/art_app/urls.py
@@ -8,16 +8,10 @@ urlpatterns = [
     path('sucsess', views.sucsess), # to render appropriate template for artist/customer
     path('add_item', views.add_item), # renders add item page for artist
     path('create_art', views.create_art), # adds artwork to DB
-<<<<<<< HEAD
-    path('add_item_img/<int:id>', views.add_item_img), # renders add image form
-    path('create_item_img/<int:id>', views.create_item_img), # saves image to DB
-
-=======
     
     path('artwork_info/<artwork_id>', views.artwork_info), # render a one artwork details page
     path('buy_artwork/<artwork_id>', views.buy_artwork),
     path('add_review/<artwork_id>', views.add_review),
     path('show_artist_profile/<artwork_id>', views.show_artist_profile),
     
->>>>>>> b7c1ed15e38e7a8162abc4207b60bf9b9853585a
 ]
